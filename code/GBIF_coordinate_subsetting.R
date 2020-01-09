f99825582ace620534c2878c7681f886616b6cdf
Beth Gerstner <bgerstner90@gmail.com>
2020-01-09 16:08:53 -0500

    rat species outliers
    note on rat species falling well into North America and within oceans (probably due to ships?)

--- code/GBIF_coordinate_subsetting.R
+++ code/GBIF_coordinate_subsetting.R
@@ -18,7 +18,8 @@ CS_americas <-subset(world, SUBREGION %in% c('5','13'))
 plot(CS_americas)
 
 #plot points to inspect
-points(occ.sp)
+points(occ.sp) 
+writeOGR(occ.sp, ".","mammal_points.shp", driver="ESRI Shapefile")
 
 #plot points over the IUCN Mexico northern limit file 
 setwd("/Volumes/GoogleDrive/My Drive/neotropical_frugivores/Data/MX_northern_limit_shapefile")
@@ -26,5 +27,12 @@ mx_northern_lim <-readOGR(".", "CSA_frugivore_union")
 plot(mx_northern_lim)
 points(occ.sp)
 
-#subset points to retain those within mx_northern_lim
+#There are some rat species that are extremely widely distributed and I will have to take them out of the anaylsis - didn't get filtered out of the centroid analysis--- need to check this. 
+#1) check to see what the IUCN range map for those rat species looks like... is the GBIF data very different from this map? If so, this is why it passed through the IUCN centroid test and wasn't incorporated into the shapefile. Check for the species at each step.
+
+
+
+
+#subset points to retain those within mx_northern_lim2
+
 
